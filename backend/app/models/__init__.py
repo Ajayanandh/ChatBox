42a397c72f6e4c59f263a51a213da0bbd16b309b
Ajay <ajayanandh000@gmail.com>
2026-09-04 20:17:42 +0530

    Delete backend directory

--- backend/app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from app.models.user import User
-from app.models.conversation import Conversation
-from app.models.message import Message
-from app.models.memory import Memory
-from app.models.document import Document
-
-__all__ = ["User", "Conversation", "Message", "Memory", "Document"]
